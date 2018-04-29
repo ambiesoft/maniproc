7881a0e4bbef9b5c4002eebe24cc5ce1dac88bf1
zgjEr <zgjEr@THEXP>
2018-04-29 23:53:05 +0900

    Add maniprocw and zzzDist

--- common/Header.h
+++ common/Header.h
@@ -1,3 +1,8 @@
 #pragma once
 
-DllExport int workmain();
+#ifdef MANIPROC_WORK
+#define DllExport __declspec( dllexport )
+#else
+#define DllExport __declspec( dllimport )
+#endif
+DllExport int workmain(bool bGui);
